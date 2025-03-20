0f33fda1ef4fbdf541fbaaf4862b06abb8fd1767
Omar TOP <106180523+OmarBalita@users.noreply.github.com>
2025-03-20 15:36:14 +0200

    Finished

--- praclang/Build/AIInterface.gd
+++ praclang/Build/AIInterface.gd
@@ -41,7 +41,7 @@ func send_message(message: String) -> void:
 
 func request_ai() -> void:
 	var headers = [
-		"Authorization: Bearer " + API_KEY2,
+		"Authorization: Bearer " + API_KEY,
 		"Content-Type: application/json"
 	]
 	var body = {
